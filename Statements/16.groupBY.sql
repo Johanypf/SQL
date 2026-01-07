672520acd6b4c48e24e754fa0091a474b93bd1c1
Johany Florez <johanypenaflorez@Johanys-Laptop.local>
2026-01-06 20:27:53 -0500

    queries

--- Statements/16.groupBY.sql
+++ Statements/16.groupBY.sql
@@ -6,3 +6,12 @@ SELECT COUNT(age),age FROM Users GROUP BY age;
 
 SELECT COUNT(age) AS "Cantidad",age FROM Users GROUP BY age;
 SELECT COUNT(age) AS "Cantidad",age FROM Users GROUP BY age order by age desc;
+
+
+
+
+
+
+SELECT usuarios.nombre, COALESCE(SUM(pedidos.total), 0) AS total_gastado FROM usuarios
+LEFT JOIN pedidos ON usuarios.id = pedidos.usuario_id
+GROUP BY usuarios.nombre ;
--- Statements/17_Having.sql
+++ Statements/17_Having.sql
@@ -1,1 +1,10 @@
 
+
+
+SELECT usuarios.nombre,
+       COALESCE(SUM(pedidos.total), 0) AS total_gastado
+FROM usuarios
+LEFT JOIN pedidos ON usuarios.id = pedidos.usuario_id
+GROUP BY usuarios.nombre
+HAVING COALESCE(SUM(pedidos.total), 0) > 2000;
+ß
